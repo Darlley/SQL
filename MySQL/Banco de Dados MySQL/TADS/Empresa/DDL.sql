dda765023df74a9a66935da1832267f0982c563a
Darlley <37590954+Darlley@users.noreply.github.com>
2019-10-17 18:38:58 -0400

    Update and rename MySQL/Banco de Dados MySQL/TADS/ex01.sql to MySQL/Banco de Dados MySQL/TADS/Empresa/DDL.sql

--- MySQL/Banco de Dados MySQL/TADS/Empresa/DDL.sql
+++ MySQL/Banco de Dados MySQL/TADS/Empresa/DDL.sql
@@ -1,12 +1,16 @@
 create database empresa;
+drop database empresa;
 create table Departamento (
 	codigo int primary key not null,
     nomeDepto varchar(20) not null,
-    codGerente int not null,
+    codGerente int,
     GerDtInicio varchar(8)
 );
+select * from departamento;
+alter table departamento ADD FOREIGN KEY (codGerente) REFERENCES empregado(codigo);
+
 create table Empregado (
-	codigo int primary key not null,
+	codigo int primary key,
     nomeIni varchar(10) not null,
     nomeInt varchar(1),
     nomeFin varchar(10) not null,
@@ -16,9 +20,12 @@ create table Empregado (
     salario double,
     codSuper int,
     codDpto int not null,
-    foreign key(codigo)
+    foreign key(codDpto)
     references Departamento(codigo)
 );
+drop table empregado;
+select * from empregado;
+
 create table Projeto (
 	codProjeto int primary key not null,
     nome varchar(20) not null,
@@ -27,29 +34,30 @@ create table Projeto (
     foreign key(codDpto)
     references Departamento(codigo)
 );
+select * from projeto;
+
 create table Dependente (
-	codEmp int primary key not null,
-	foreign key(codEmp)
-    references Empregado(codigo),
+	codEmp int,
     nome varchar(10) not null,
     sexo varchar(1),
     dataNasc varchar(8),
     parentesco varchar(8)
 );
+select * from dependente;
+alter table dependente ADD FOREIGN KEY (codEmp) REFERENCES empregado(codigo);
+
 create table dpto_localizacao (
-	codDpto int primary key not null,
-	foreign key(codDpto)
-    references Departamento(codigo),
+	codDpto int,
     localizacao varchar(10)
 );
-create table trabalha_em (
-	codEmp int primary key not null,
-    foreign key(codEmp)
-    references empregado(codigo)
-);
-select * from departamento;
-select * from empregado;
-select * from projeto;
-select * from dependente;
 select * from dpto_localizacao;
+alter table dpto_localizacao ADD foreign key(codDpto) references Departamento(codigo);
+
+create table trabalha_em (
+	codEmp int,
+    codProjeto int,
+    horas double
+);
 select * from trabalha_em;
+alter table trabalha_em ADD foreign key(codEmp) references empregado(codigo);
+alter table trabalha_em ADD foreign key(codProjeto) references projeto(codProjeto);
