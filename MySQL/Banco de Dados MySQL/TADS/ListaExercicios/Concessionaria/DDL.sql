ee6c9637a68504f020aa6bf23dd8f400c2a9da70
Darlley <37590954+Darlley@users.noreply.github.com>
2019-10-29 19:39:02 -0400

    Update DDL.sql

--- MySQL/Banco de Dados MySQL/TADS/ListaExercicios/Concessionaria/DDL.sql
+++ MySQL/Banco de Dados MySQL/TADS/ListaExercicios/Concessionaria/DDL.sql
@@ -1,7 +1,7 @@
 create database concessionaria;
 
 create table automovel (
-	codigo int,
+	codigo int primary key,
     ano int,
     fabricante varchar(50),
     modelo varchar(50),
@@ -9,42 +9,38 @@ create table automovel (
     preco_tabela float
 );
 create table revendedora (
-	cnpj char(14),
-    nome varchar(50),
-    proprietario varchar(50),
-    cidade varchar(50),
-    estado char(2)
+	cnpj char(14) primary key,
+	nome varchar(50),
+	proprietario varchar(50),
+	cidade varchar(50),
+	estado char(2)
 );
 create table consumidor (
 	identidade int,
-    nome varchar(50),
-    endereco varchar(50),
-    cpf varchar(11)
+ 	nome varchar(50),
+	endereco varchar(50),
+	cpf varchar(11)
 );
 create table estoque (
+	codEstoque int primary key,
 	codigo int,
-    ano int,
-    cnpj char(14),
-    quantidade int,
-    FOREIGN KEY (codigo)
-    REFERENCES automovel(codigo),
-    FOREIGN KEY (ano)
-    REFERENCES automovel(ano),
-    FOREIGN KEY (cnpj)
-    REFERENCES revendedora(cnpj)
+	ano int,
+	cnpj char(14),
+	quantidade int
+	check (quantidade > 1 and quantidade < 10)
 );
+alter table estoque ADD FOREIGN KEY (codigo) REFERENCES automovel(codigo);
+alter table estoque ADD FOREIGN KEY (ano) REFERENCES automovel(ano); /*NÃO VAI*/
+alter table estoque ADD FOREIGN KEY (cnpj) REFERENCES revendedora(cnpj);
 
 create table negocio (
 	codigo int,
-    ano int,
-    identidade int,
-    cnpj char(14),
-    data_venda varchar(8),
-    preco_venda float,
-    FOREIGN KEY (codigo)
-    REFERENCES automovel(codigo),
-    FOREIGN KEY (ano)
-    REFERENCES automovel(ano),
-    FOREIGN KEY (identidade)
-    REFERENCES consumidor(identidade)
+	ano int,
+	identidade int,
+	cnpj char(14),
+	data_venda varchar(8),
+	preco_venda float
 );
+alter table negocio ADD FOREIGN KEY (codigo) REFERENCES automovel(codigo);
+alter table negocio ADD FOREIGN KEY (ano) REFERENCES automovel(ano); /*NÃO VAI*/
+alter table negocio ADD FOREIGN KEY (identidade) REFERENCES consumidor(identidade); /*NÃO VAI*/
