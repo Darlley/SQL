16fbdc276436bd420e19628753376ff4cd291f71
Darlley <37590954+Darlley@users.noreply.github.com>
2019-10-29 20:16:01 -0400

    Create ex04.sql

---
+++ MySQL/Banco de Dados MySQL/TADS/ListaExercicios/ex04.sql
@@ -0,0 +1,22 @@
+create database exercicio04;
+
+create table cidade (
+	CodCidade int primary key not null,
+    nomeCidade varchar(15)
+);
+
+insert into cidade values	(1,"Campo Grande"),
+							(2, "Dourados"),
+							(3, "Corumbá"),
+							(4, "Três Lagoas"),
+							(5, "Ponta Porã"),
+							(6, "Rio Verde"),
+							(7, "Porto Murtinho"),
+							(8, "Bonito"),
+							(9, "Paranaíba");
+select * from cidade;
+
+update cidade
+set nomeCidade = "Cassilândia"
+where CodCidade = 9;
+select * from cidade;
